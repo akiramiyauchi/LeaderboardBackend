b8bb4772865fb97c39851611a5ee0b086e6deee5
akiramiyauchi <akiraamiyauchi@gmail.com>
2024-12-22 17:33:06 +0900

    Update leaderboard.js

--- netlify/functions/leaderboard.js
+++ netlify/functions/leaderboard.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-exports.handler = async function(event, context) {
+export async function handler(event, context) {
     const APP_ID = "7951375894910515";
     const APP_SECRET = "a7fa72a764bb60aa20513e272fceeee3";
     const ACCESS_TOKEN = `OC|${APP_ID}|${APP_SECRET}`;
@@ -19,4 +19,4 @@ exports.handler = async function(event, context) {
             body: JSON.stringify({ message: "エラーが発生しました", error: error.toString() }),
         };
     }
-};
+}
