4ae41339c804bbd8df642a0b523a88e0ba0901c1
akiramiyauchi <akiraamiyauchi@gmail.com>
2024-12-22 17:58:53 +0900

    Update leaderboard.js

--- netlify/functions/leaderboard.js
+++ netlify/functions/leaderboard.js
@@ -1,22 +1,26 @@
-import axios from "axios";
+const fetch = require("node-fetch"); // node-fetch@2 を使用
 
-export async function handler(event, context) {
-    const APP_ID = "7951375894910515";
-    const APP_SECRET = "a7fa72a764bb60aa20513e272fceeee3";
+exports.handler = async function (event, context) {
+    const APP_ID = "7951375894910515"; // あなたのAPP_ID
+    const APP_SECRET = "a7fa72a764bb60aa20513e272fceeee3"; // あなたのAPP_SECRET
     const ACCESS_TOKEN = `OC|${APP_ID}|${APP_SECRET}`;
     const API_URL = `https://graph.oculus.com/${APP_ID}/leaderboards?api_name=HIGH_SCORE&fields=entries{rank,user{name},score}`;
 
     try {
-        const response = await axios.get(`${API_URL}&access_token=${ACCESS_TOKEN}`);
-        const data = response.data; // axiosではレスポンスデータはdataプロパティに格納されます。
+        // APIリクエスト
+        const response = await fetch(`${API_URL}&access_token=${ACCESS_TOKEN}`);
+        const data = await response.json();
+
+        // 成功時のレスポンス
         return {
             statusCode: 200,
             body: JSON.stringify(data),
         };
     } catch (error) {
+        // エラー時のレスポンス
         return {
             statusCode: 500,
             body: JSON.stringify({ message: "エラーが発生しました", error: error.toString() }),
         };
     }
-}
+};
