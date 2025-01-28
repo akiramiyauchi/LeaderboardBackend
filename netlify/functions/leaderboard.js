ebc333491c12c17c345a9c8d77dcc7b17fb5ab36
akiramiyauchi <akiraamiyauchi@gmail.com>
2025-01-28 11:15:00 +0900

    Update leaderboard.js

--- netlify/functions/leaderboard.js
+++ netlify/functions/leaderboard.js
@@ -4,7 +4,7 @@ exports.handler = async function (event, context) {
     const APP_ID = "7951375894910515";
     const APP_SECRET = "a7fa72a764bb60aa20513e272fceeee3";
     const ACCESS_TOKEN = `OC|${APP_ID}|${APP_SECRET}`;
-    const API_URL = `https://graph.oculus.com/leaderboard_entries?api_name=HIGH_SCORE&fields=rank,user{alias},score`;
+    const API_URL = `https://graph.oculus.com/leaderboard_entries?api_name=HIGH_SCORE&fields=rank,user{alias,display_name},score`;
 
     // 全ページを取得する関数
     async function fetchAllPages(url, results = []) {
